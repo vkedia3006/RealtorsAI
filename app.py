3062f0a5225a847230297439160d56a18000b42c
vkedia <vkedia@uwaterloo.ca>
2025-03-31 20:41:18 -0400

    Fix

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import requests
 from fastapi import FastAPI, Query, status
-from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -10,10 +11,22 @@ FB_APP_SECRET = "db457a2ea5cf9a2112e7e79bcf95115e"
 FB_REDIRECT_URI = "https://4e8d-199-247-192-103.ngrok-free.app"
 VERIFY_TOKEN = "mysecrettoken123"
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class WebhookData(BaseModel):
     object: str
     entry: list
     
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to the homepage!"}    
+
 @app.get("/api/test")
 async def test():
     print("HELLO WORLD")
